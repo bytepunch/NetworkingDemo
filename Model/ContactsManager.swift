4b1241b8172f660d22f61bb8f16bb593753ed348
Jörg Klausewitz <joerg@bytepunch.com>
2019-09-04 11:39:38 +0200

    MVC structured

---
+++ Model/ContactsManager.swift
@@ -0,0 +1,9 @@
+//
+//  ContactsManager.swift
+//  NetworkingDemo
+//
+//  Created by Jörg Klausewitz on 04.09.19.
+//  Copyright © 2019 Jörg Klausewitz. All rights reserved.
+//
+
+import Foundation
--- ViewController/ViewController.swift
+++ ViewController/ViewController.swift
@@ -10,13 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    // MARK: - Properties
     
+    // MARK: - Outlets
+    @IBOutlet weak var connectButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    
+    // MARK: - Actions
+    
+    @IBAction func didClickOnConnectButton(_ sender: UIButton) {
+        
+        
+        
+    }
+    
 
 }
 
